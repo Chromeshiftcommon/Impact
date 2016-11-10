97425efccbe2dfc58812efdac3b1b7b1b718794a
Chromeshiftcommon <farrvincedev@yahoo.com>
2016-11-10 11:54:22 +0100

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,23 @@
+<?php
+include_once 'common.php';
+include_once 'functions.php';
+
+$replyto = $_POST["email"];
+$tel = $_POST["telnum"];
+$msgold = $_POST["message"];
+$msggood = wordwrap($msgold, 70);
+$name = $_POST["firstname"] . " " . $_POST["lastname"];
+$to = "egybizonyosember@gmail.com";
+$additional_headers = "Date de contact: \r\n" .
+                      "Nume:" . $name . "\r\n" .
+                      "Nr. telefon:" . $tel . "\r\n" .
+                      "Email:" . $replyto;
+
+$result = mail($to, "Anunt de la Impact Mode", $msggood, $additional_headers);
+if ($result){ renderheader("Succes!", ""); $text = "Email-ul dvs. a fost trimis cu succes!";}
+else { renderheader("Ooops!", ""); $text = "A apărut a problemă, vă rog să încercați din nou! ";}
+?>
+
+<?php echo $text; ?>
+<a href="index.php"> Înapoi la pagina de start </a>
+<?php renderfooter() ?>
