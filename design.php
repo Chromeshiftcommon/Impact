6564ca69e0579ffebaad9040789c5f12170f333a
Chromeshiftcommon <farrvincedev@yahoo.com>
2016-11-10 12:02:32 +0100

    Add files via upload

--- design.php
+++ design.php
@@ -1,7 +1,7 @@
 <?php
 include_once 'common.php';
 include_once 'functions.php';
-renderheader("Design your own shirt! ", "design");
+renderheader("Design your own shirt! ");
  ?>
         <script type="text/javascript">
         function changeColor(color)
@@ -13,19 +13,15 @@ renderheader("Design your own shirt! ", "design");
             <?php echo 'Design your own shirt! ' ;?>
         </h1>
         <div class="container-fluid">
-        <ul class="row-img nobullet">
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('black');"><img style="width:100px; height:100px;" src="ruhaszinek/black.jpg"></li>
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('blue');"><img style="width:100px; height:100px;" src="ruhaszinek/blue.jpg"></li>
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('green');"><img style="width:100px; height:100px;" src="ruhaszinek/green.jpg"></li>
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('orange');"><img style="width:100px; height:100px;" src="ruhaszinek/orange.jpg"></li>
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('red');"><img style="width:100px; height:100px;" src="ruhaszinek/red.jpg"></li>
-          <li id="myImgDesign" class="col-lg-2 col-md-3 col-sm-4 col-xs-12" onclick="changeColor('yellow');"><img style="width:100px; height:100px;" src="ruhaszinek/yellow.jpg"></li>
+        <ul>
+          <li onclick="changeColor('black');">Black</li>
+          <li onclick="changeColor('blue');">Blue</li>
+          <li onclick="changeColor('green');">Green</li>
+          <li onclick="changeColor('orange');">Orange</li>
+          <li onclick="changeColor('red');">Red</li>
+          <li onclick="changeColor('yellow');">Yellow</li>
         </ul>
-        </div>
-        <br>
-        <div class="container-fluid">
         <img id ="pic" src="ruhaszinek/black.jpg">
         </div>
-        <br><br>
 
 <?php renderfooter(); ?>
