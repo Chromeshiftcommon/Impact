11d23ed70e4ee1ff5f5523f7716799d3803370c3
Chromeshiftcommon <farrvincedev@yahoo.com>
2016-12-02 17:20:02 +0100

    Add files via upload

--- languages/lang.en.php
+++ languages/lang.en.php
@@ -20,6 +20,7 @@ $lang['GALLERY'] = 'Gallery';
 $lang['CONTACT'] = 'Contact';
 $lang['DESIGN'] = 'Shirt design';
 $lang['QUALITY'] = 'Quality';
+$lang['FACTS'] = 'Collections';
 
 $lang['TEST'] = 'ENGLISH';
 
@@ -39,10 +40,44 @@ $lang['MAIL_SUCCESS_MESSAGE'] = 'The email has been sent successfully';
 $lang['MAIL_ERROR'] = 'ERROR!';
 $lang['MAIL_ERROR_MESSAGE'] = 'Your email was not sent!';
 
+// Stuff for design.php
+$lang['DESIGNCONTENT'] = 'Design your own shirt!';
+
 // Stuff for index.php
 $lang['INDEXTEXT'] = 'No content yet, sorry!';
+$lang['PRESENTATION'] = 'Tootal fabrics. Your local partner!';
+$lang["INDEXCONTENT"] = '
 
-// Stuff for catalog.php
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <ul class="ctext1">
+            <li>Excellent worldwide logistics, guaranteeing quick deliveries</li>
+            <li>Supplying to more than 80 countries</li>
+            <li>Excellent range of fabrics for fashion developed in conjuction with leading marketing advisers and Italian design studios</li>
+            <li>Minimum 10 million meters constantly in stock</li>
+            <li>Long established and reliable partner since 1972</li>
+            <li>Long track record in textiles, more than 40 years of experience</li>
+            <li>Strong network of high quality suppliers</li>
+            <li>Long-term strategic supply side partners</li>
+            <li>Production in state of the art textile mills in Asia</li>
+            <li>Experienced staff in all key disciplines</li>
+        </ul>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" width="530" height="315" src="https://www.youtube.com/embed/c-5nc_1dkSY" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <br>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="ember-responsive-item" width="530" height="315" src="https://www.youtube.com/embed/ivlaz1KZpmg" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <br>
+      </div>
+    </div>
+';
+
+/* { Unused content stuff, but maybe once useful
+ Stuff for catalog.php
 $lang['SHIRT'] = 'O parte substanţială şi foarte importantă din gama firmei o reprezintă colecţia vastă de materiale pentru cămăşi şi bluze din bumbac 100% şi amestec de polyester cu bumbac, bumbac/lycra.';
 $lang['CORPWEAR'] = 'Oferim o gamă largă de materiale pentru echipamentele de lucru diferitelor organizaţii guvernamentale şi neguvernamentale, cum ar fi armata, poliţia, poşta, vama, căile ferate şi linii aeriene, precum şi companiilor de confecţii.';
 $lang['RAW'] = 'Impact Mode oferă materiale nefinisate care ţintează cumpărători competitivi de volume mari, cu un accent constant pe calitate ridicată la preţuri atractive.';
@@ -101,5 +136,38 @@ Gama largă de ţesături în stoc la furnizorul nostru a fost realizat în str�
 <br><br>
 Ţesăturile pentru buzunare sunt livrate în diverse combinaţii şi execuţii.
 <br><br>
-(pentru vizionarea cartelelor, click pe poză)';
+(pentru vizionarea cartelelor, click pe poză)'; }*/
+// Stuff for quality.php
+
+$lang['QUALITYCONTENT'] = '
+<ul class="ctext1">
+    <li>Quality assurance during all stages of the supply chain</li>
+    <li>High and consistent quality level of all fabrics</li>
+    <li>Top quality cotton and blended yarns used only</li>
+    <li>Quality controls in accredited laboratories</li>
+    <li>Mills with 100% quality of fabrics inspection. This combined with strict in-house quality control procedures in the Netherlands</li>
+    <li>Daily contact and frequent, continuous presence at the mills</li>
+    <li>Oeko-Tex certification for our complete product range</li>
+    <li>Guaranteeing longterm colour consistency</li>
+    <li>Extensive database, including testreports, datasheets, etc</li>
+</ul>
+
+<p id="more" class="ctext1 container-fluid">Read more</p>
+<div id="hidden" hidden class="container-fluid ctext1">
+<i>“A high and consistent quality level”</i> <br><br>
+
+The number of preferred suppliers in Asia, with their state of the art mills, are a selective group. They have experienced staff in all disciplines from spinning and, weaving till finishing. During all steps of the production process in depth quality control guarantees that Tootal fabrics (Holland) b.v. over the years supplies a high and consistent quality level. This is not only key for the fashion shirt customers but certainly also for those clients who supply shirts for uniform tenders (corporate identity shirt tenders).
+<br><br>
+We follow AEO and OEKO-tex certification, that’s why we are sure that everybody is pleased with our fabrics. Our good  and daily contact, support our strong worldwide network of mills and suppliers.
+</div>
+';
+
+// Stuff for catalog.php and news in a different file - lang.ro.news.php
+// Also stuff for facts.php - lang.ro.facts.php
+
+// Stuff for gallery.php
+$lang['GALLERYMORE'] = 'Fabric designs gallery
+<br>
+<i>Just as a quick reference we have made a gallery based on a small selection of fabric designs which are part of our standard and seasonal collections. </i>'
+
 ?>
---
+++ languages/lang.ro.facts.php
@@ -0,0 +1,83 @@
+<?php
+/*
+------------------
+Language: Romanian
+------------------
+*/
+
+$lang['FACTS1'] = '
+<h3>Panama collection</h3>
+
+Panama, also called basket weave, is a simple criss-cross weaving construction. The warp and weft threads are interlaced, were the weft group crosses the equal number of warp threads, by over and under, mostly used for men and women shirts and dresses. Our panama articles are available in 100% cotton blends and also in stretch in a wide range of colours.
+<br><br>
+<h3>Taffeta collection</h3>
+
+Taffeta is a favourite choice for lining. It’s a shiny medium weight plain weave fabric. It has a ribbed structure in a diagonal direction. Take a look in our Tootal collection to see our taffeta fabric range.
+<br><br>
+<h3>Print collection</h3>
+Uniquely designed Tootal fabrics! Fabric printing is a process where the designs will be printed on the complete fabric. Engraved plates, rollers or other screens are used to place the different colours on the fabric. This makes it easy to create fabrics with special designs. We have fabrics with attractive looks in different compositions and constructions.
+
+';
+$lang['FACTS2'] = '
+
+<h3>Plain collection</h3>
+Plain weave for fashion and furnishing fabrics. Plain has the most basic weave construction there is. The construction of the warp and the weft weave are aligned, what makes it a simple pattern. It is a strong weave which is easy to print and finish. You will find the plain construction in many of our fabrics.
+<br><br>
+<h3>Yarn dye collection</h3>
+A yarn dye fabric is a fabric where the yarn has been dyed before its woven into fabrics. Using this method makes the fabric more colourfast. You find yarn dye fabrics in different weaves and patterns like stripes and checks in Cotton Polyester mixing.
+<br><br>
+<h3>Two fold collection</h3>
+Let’s twist! Two fold refers to the yarn used in weaving the fabric, because two yarns are combined and twisted to form the thread. There are two methods to twist the yarns; clockwise and anti-clockwise. The clockwise direction, also called Z-twist and the anti-clockwise method, is called S-twist. The direction of the yarn and twill ensures the finished material is softer than fabrics created with a corresponding yarn and twill weave. By twisting two or more yarns, the fabric will be much stronger. Take a look into our wide range of Tootal two fold fabrics in different compositions, constructions and colours.
+
+';
+$lang['FACTS3'] = '
+<h3>Slub collection</h3>
+A fabric with an imperfection. A slub fabric has is name to its irregular yarns. Some yarns of a slub fabric have a fuller place in yarn. This slub is produced by changing the speed of the bottom rollers on the spinning frame. We have a very nice Tootal slub fabric collection in different compositions and colours.
+<br><br>
+<h3>Stretch collection</h3>
+Strong and flexible. Stretch. The composition of Tootal Stretch fabric is a mix of cotton with stretch yarns or cotton, stretch and polyamide yarns. The elastic stretch yarns gives the fabric extra movement. You find this fabric in different weaves, patterns and colours in our Tootal collection.
+<br><br>
+<h3>Chambray/Denim collection</h3>
+Chambray fabric has a denim look but with the advantage of being colour fast and not fade out. It is a plain weave fabric with coloured warps and a contrast colour weft. The use of thicker yarns by chambray construction is giving a rugged look.  In addition we offer twill chambray stretch and chambray printed. Denim fabric commonly known in Jeanswear it has a ‘fade out ‘or  distressed look after washing and has the heavier more rugged look associated with casual wear collections. Suggested wash recipes are available to assist clients achieve the casual look. Fabrics with a chambray/ denim....
+<br><br>
+<a class="incontent" href="denimcoll.php">Read this article</a>
+';
+$lang['FACTS4'] = '
+<h3>Fil à fil collection</h3>
+
+A fil à fil fabric construction looks like a game of warp and weft, wherein the two different colours variously lie next to each other. The fil à fil fabrics are presented in 100% cotton and cotton polyester mixing in a wide range of colours.
+<br><br>
+<h3>Herringbone collection</h3>
+The herringbone fabric has a woven-in construction. It’s a combination of twill with a v-weave pattern like the skeleton of a fish. You will find the herringbone plain fabrics in cotton and polyester cotton blends in our Tootal collection.
+<br><br>
+<h3>Poplin collection</h3>
+A versatile tightly woven fabric with a fine, smooth and flat appearance due to the warps are more tightly packed than the weft. Our poplin fabric collection is available in 100% cotton and Cotton Polyester blends.
+';
+$lang['FACTS5'] = '
+<h3>Satin collection</h3>
+Satin is a weave type that is very close and tight woven, were the warp yarns have dominance over the weft. The structure of the weave gives an unbroken smooth front side effect. The front of the fabric can have a little shimmering look. You will find our satin fabric products in Cotton and Cotton Polyester mixing in different colours.
+<br><br>
+<h3>Oxford collection</h3>
+The oxford fabric construction is a combination of two warp yarns, mostly with a different colour weft. You have different oxford weaves; with rougher and fine textures. The oxford fabrics have a soft feel and will be used for more casual or sporty dress shirts. In our oxford fabric collection are a lot of fascinating patterns and colours in 100% cotton and Cotton Polyester blends.
+<br><br>
+<h3>Twill collection</h3>
+Twill has a diagonal pattern of similar lines on the face of the fabric. It has a construction with a variety of different interlines. This allows for a characteristic pattern. Twill fabrics are generally soft and more wrinkle resistant than plain woven fabrics. You find our twill collection in 100% cotton and cotton polyester blends.
+';
+$lang['FACTS6'] = '
+<h3>Dobby collection</h3>
+Dobby fabric is a woven fabric with a special interest weave. The raised threads produce small symmetrical patterns that repeat throughout the fabric. The complex weave designs such as stripes, checks, and other geometric patterns are woven into them. Dobby fabrics can be woven from a range of yarns as cotton, rayon or silk with small, thick or fluffy fibres. The advantage of a dobby is the minimal crease, so it can perfectly be used for shirts and blouses.
+<br><br>
+<h3>Fabric constructions</h3>
+The fabrics in our collection have a wide range of construction. The way how the warp and weft threads interlace with each other, the composition and colour gives the fabric a decorative look. In our collection you can find amongst others: Dobby; Twill; Oxford; Satin; Poplin; Herringbone; Fil à fil; Chambray; Stretch; Slub; Two folds; Yarn dye; Plain; Panama; Print; Taffeta.
+<br><br>
+<h3>Fabrics finish types</h3>
+The finishing of the fabric is the final step in fabric production. It change its appearance, hand feel or performance. The finishing is meant to make the fabric a usable material and makes it more suitable for its end use. We have a couple of finish types we use in our fabric collection. Besides our ‘basic’ regular finish, we also have: Easy care (Ammonia, Moistcure, Symphony): a wrinkle free, creasing free and iron free finish; Silky: gives the fabric a soft hand feel; Teflon: is a stain and moisture repellent finish; Water repellent: is a water repellent finish.
+';
+$lang['DENIMCOLL'] = '
+<div class="newstitle">Chambray/Denim collection</div>
+<p><p>Chambray fabric has a denim look but with the advantage of being colour fast and not fade out. It is a plain weave fabric with coloured warps and a contrast colour weft. The use of thicker yarns by chambray construction is giving a rugged look.  In addition we offer twill chambray stretch and chambray printed.</p>
+<p>Denim fabric commonly known in Jeanswear it has a ‘fade out ‘or  distressed look after washing and has the heavier more rugged look associated with casual wear collections.</p>
+<p>Suggested wash recipes are available to assist clients achieve the casual look.</p>
+<p>Fabrics with a chambray/ denim construction are available in 100% cotton in different colours.</p>
+';
+?>
